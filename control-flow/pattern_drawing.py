b25b7b9f7b6d26d0b36891042cf051a3529d34e5
Adaobi Ibekwe <adaodinukwe@gmail.com>
2025-10-05 22:52:46 +0100

    added pattern drawing file added

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,9 @@
-size = int(input(" Enter the size of the pattern:"))
+size = int ( input ( " Enter the size of the pattern: " ) )
 i = 0
 while i < size:
     for j in range (size):
         print("*", end="")
     print()
     i = i + 1
-    
+
     
