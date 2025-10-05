61f8ef0586cbc9d703a5a32631ff3d65013e5802
Adaobi Ibekwe <adaodinukwe@gmail.com>
2025-10-05 22:14:19 +0100

    added match case calculator file edited4

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = input("Enter the first number:")
 num2 = input("Enter the second number:")
-operation = input ("Choose the operation (+, -, *, /):.")
+operation = input ( "Choose the operation (+, -, *, /): " )
 match operation:
     case "+":
         result = num1 + num2
