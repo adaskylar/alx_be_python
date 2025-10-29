239055609255f6d48ab648fd2207ba373c98b659
Adaobi Ibekwe <adaodinukwe@gmail.com>
2025-10-29 03:09:55 +0100

    Add arithmetic operations function and main script

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1: float, num2: float, operation: str):
+
+    op = operation.strip().lower()
+
+    if op == "add":
+        return num1 + num2
+    elif op == "subtract":
+        return num1 - num2
+    elif op == "multiply":
+        return num1 * num2
+    elif op == "divide":
+        
+        # handle division by zero
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation. Use 'add', 'subtract', 'multiply', or 'divide'."
